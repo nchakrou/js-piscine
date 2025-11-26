f481de8fc4c6a6ec5b728db3091097650a445643
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-11-26 12:20:09 +0100

    first

---
+++ declarations.js
@@ -0,0 +1,17 @@
+const escapeStr = "`\\/\"'";
+const arr = [4, "2"];
+const obj = {
+  str: "",
+  num: 0,
+  bool: false,
+  undef: undefined,
+};
+const nested = {
+  arr: [4, undefined, "2"],
+  obj: {
+    str: "",
+    num: 0,
+    bool: false,
+  },
+};
+Object.freeze(arr, obj, nested, nested.arr, nested.obj);
