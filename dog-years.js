0eb8aa351106f76d02c75c6a3f08254fb9ba9218
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-11-26 15:46:00 +0100

    first

--- dog-years.js
+++ dog-years.js
@@ -2,19 +2,19 @@ function dogYears(name, age) {
   const Years = (age / 60 / 60 / 24 / 365) * 7;
   switch (name) {
     case "mercury":
-      return Years * 0.2408467;
+      return Years / 0.2408467;
     case "venus":
-      return Years * 0.61519726;
+      return Years / 0.61519726;
     case "mars":
-      return Years * 1.8808158;
+      return Years / 1.8808158;
     case "jupiter":
-      return Years * 11.862615;
+      return Years / 11.862615;
     case "saturn":
-      return Years * 29.447498;
+      return Years / 29.447498;
     case "uranus":
-      return Years * 84.016846;
+      return Years / 84.016846;
     case "neptune":
-      return Years * 164.79132;
+      return Years / 164.79132;
   }
   return Years;
 }
