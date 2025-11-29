daf65da2c7a726142f6bae66e792e4e46de0a57c
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-11-29 16:10:55 +0100

    first

--- unbreakable.js
+++ unbreakable.js
@@ -1,27 +1,26 @@
 function split(str, sep) {
   let res = [];
   let temp = "";
-  for (let s of str) {
-    if (s === sep && temp != "") {
+  for (let i = 0; i < str.length; i++) {
+    if (str.slice(i, i + sep.length) === sep) {
       res.push(temp);
       temp = "";
       continue;
     }
-    temp += s;
+    temp += str[i];
   }
-  if (temp !== ""){
-      res.push(temp);
-
+  if (temp !== "" && temp + sep[sep.length - 1] != sep) {
+    res.push(temp);
   }
   return res;
 }
 function join(arr, sep) {
   let res = "";
   for (let i in arr) {
-    if (i !== 0) {
+    if (i !== "0") {
       res += sep;
     }
     res += arr[i];
   }
-  return res
+  return res;
 }
