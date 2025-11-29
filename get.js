6786e4925976173e54319a80a1753d0c9f578bde
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-11-29 17:59:02 +0100

    first

--- get.js
+++ get.js
@@ -3,6 +3,9 @@ function get(src, path) {
 
   let x = src;
   for (let i = 0; i < sp.length; i++) {
+    if (x[sp[i]]===undefined){
+        return undefined
+    }
     x = x[sp[i]];
   }
   return x;
