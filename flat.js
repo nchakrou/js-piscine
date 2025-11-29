372aa180baa0a09fea6fcc8e90ce52ca75ede6f1
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-11-29 20:18:24 +0100

    first

--- flat.js
+++ flat.js
@@ -4,13 +4,18 @@ function flat(arr, num = 1) {
     return arr;
   }
   while (num > 0) {
+    let solve = false
     res = [];
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
+        solve = true
         res.push(...arr[i]);
       } else {
         res.push(arr[i]);
       }
+    }
+    if (!solve) {
+        return res
     }
     arr = res;
     num--;
