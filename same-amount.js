94432426f5d3950c1d5103d5b93e0feb49a18003
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-11-30 16:04:04 +0100

    first

---
+++ same-amount.js
@@ -0,0 +1,5 @@
+function sameAmount(str, reg1, reg2) {
+  const res1 = str.match(new RegExp(reg1.source, "g"));
+  const res2 = str.match(new RegExp(reg2.source, "g"));
+  return res1.length == res2.length;
+}
