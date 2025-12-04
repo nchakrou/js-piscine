d4e1953bdd8264894e26e79c75777434a934b10e
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-04 18:22:29 +0100

    first

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,9 +1,12 @@
 function dayOfTheYear(date) {
   let newday = new Date(`${date.getFullYear()}-01-01`);
   var count = 0;
+  if (date.getFullYear() % 4 == 0) count++;
+
   while (newday <= date) {
     newday.setDate(newday.getDate() + 1);
     count++;
   }
   return count;
 }
+
