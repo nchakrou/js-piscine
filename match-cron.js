56d1e0e027095dd2027afb75eb1b32a9a1158564
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-04 17:12:55 +0100

    first

--- match-cron.js
+++ match-cron.js
@@ -1,13 +1,23 @@
-function matchCron(cron,date){
-    let dt = [];
-    dt.push(date.getMinutes(),date.getHours(),date.getDate(),date.getMonth()+1,date.getDay()+1)
-const cr = cron.split(" ")
-    for (let c in cr){
-        if (cr[c]!="*"){
-            if (cr[c]!=dt[c]){
-                return false
-            }
-        }
+function matchCron(cron, date) {
+  let dt = [];
+  dt.push(
+    date.getMinutes(),
+    date.getHours(),
+    date.getDate(),
+    date.getMonth() + 1,
+    date.getDay()
+  );
+  if (date.getDay() === 0) {
+    dt[4] = 7;
+  }
+  console.log(dt[4]);
+  const cr = cron.split(" ");
+  for (let c in cr) {
+    if (cr[c] != "*") {
+      if (cr[c] != dt[c]) {
+        return false;
+      }
     }
-    return true
+  }
+  return true;
 }
