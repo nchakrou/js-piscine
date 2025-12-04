a1ac13f8e069f5139e90c37fdbbb83193a9a41ea
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-04 14:02:06 +0100

    solve

--- get-some-time.js
+++ get-some-time.js
@@ -2,13 +2,14 @@ function firstDayWeek(number, year) {
   const date = new Date(year + "-01-01");
   date.setDate(date.getDate() + (number - 1) * 7);
   while (date.getDay() > 1) {
-    date.setDate(date.getDate() - 1);
-  }
-  if (date.getFullYear() < year) {
-    return `01-01-${year}`;
-  }
+      date.setDate(date.getDate() - 1);
+    }
+    if (date.getFullYear() < year) {
+        return `01-01-${year}`;
+    }
   if (date.getDate() < 10) {
-    return `0${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    return `0${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
   }
-  return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+  return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
 }
+
