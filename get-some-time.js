99db6f88b55bc3a86ea79c4282e18849f2aedcd7
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-04 13:54:49 +0100

    solve

--- get-some-time.js
+++ get-some-time.js
@@ -1,14 +1,14 @@
-function firstDayWeek(number = 50, year = "2021") {
+function firstDayWeek(number, year) {
   const date = new Date(year + "-01-01");
   date.setDate(date.getDate() + (number - 1) * 7);
   while (date.getDay() > 1) {
     date.setDate(date.getDate() - 1);
   }
-  
-  console.log(date.getMonth());
-  if (date.getDate()<10){
-    return `0${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+  if (date.getFullYear() < year) {
+    return `01-01-${year}`;
   }
-  return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+  if (date.getDate() < 10) {
+    return `0${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+  }
+  return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
 }
-console.log(firstDayWeek());
