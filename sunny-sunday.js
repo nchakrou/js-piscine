b71f83f303374ccfd54051497cc57c8ae6582c23
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-04 19:15:35 +0100

    first

---
+++ sunny-sunday.js
@@ -0,0 +1,16 @@
+function sunnySunday(date) {
+    def = new Date("0001-01-01")
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let d = 24*60*60*1000
+  let day = (date.getTime()-def.getTime())/d
+
+ return days[day%6] 
+}
+
