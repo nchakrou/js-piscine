d8a80c1ec7f6e196883a314be851b843556dec13
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-04 18:25:43 +0100

    solve

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,12 +1,14 @@
 function dayOfTheYear(date) {
   let newday = new Date(`${date.getFullYear()}-01-01`);
   var count = 0;
-  if (date.getFullYear() % 4 == 0) count++;
-
   while (newday <= date) {
     newday.setDate(newday.getDate() + 1);
     count++;
   }
+  if (count == 0){
+    count++
+  }
   return count;
 }
+console.log(dayOfTheYear(new Date('0001-01-01')));
 
