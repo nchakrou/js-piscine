b515e4a1af7b80cc438939495d00959bac4ae701
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-04 17:54:53 +0100

    first

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,6 +1,6 @@
 function countLeapYears(date) {
   let count = 0;
-  for (let i = 1; i <= date.getFullYear(); i++) {
+  for (let i = 1; i < date.getFullYear(); i++) {
     if (i % 4 == 0 && i % 100 != 0) {
       count++;
     } else if (i % 4 == 0 && i % 100 == 0 && i % 400 == 0) {
