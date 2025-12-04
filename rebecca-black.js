35112fb4778874006ed755df84b145941ba17d34
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-04 16:03:17 +0100

    first

---
+++ rebecca-black.js
@@ -0,0 +1,15 @@
+function isFriday(date) {
+    return date.getDay() === 5;
+}
+function isWeekend(date) {
+    return date.getDay() === 6 || date.getDay() === 0;
+}
+function isLeapYear(date) {
+    return date.getFullyear() % 4 === 0;
+}
+function isLastDayOfMonth(date) {
+    const next = new Date(date);
+    next.setDate(next.getDate() + 1);
+    return next.getDate() === 1;
+}
+console.log(isLastDayOfMonth(new Date("2020-01-31")));
