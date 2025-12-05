4b345e1c0b4fb786778d9224e4bb8d31d11a2650
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-05 18:02:10 +0100

    first

--- mapper.js
+++ mapper.js
@@ -1,14 +1,14 @@
 function map(arr,fn){
     let res =[];
     for (let i =0;i<arr.length;i++){
-        res.push(fn(arr[i]))
+        res.push(fn(arr[i],i,arr))
     }
     return res
 }
 function flatMap(arr,fn){
     let res =[];
     for (let i = 0 ;i<arr.length;i++){
-        let temp = fn(arr[i])
+        let temp = fn(arr[i],i,arr)
         if (Array.isArray(arr)){
             res.push(...temp)
         }else{
