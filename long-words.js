15ea68e93b401c46e3e6ea8961cfee92893d4094
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-05 17:23:03 +0100

    first

--- long-words.js
+++ long-words.js
@@ -3,7 +3,8 @@ function longWords(arr) {
   return arr.every(isBelowThreshold);
 }
 function oneLongWord(arr) {
-  return arr[arr.length - 1].length >= 10;
+ const isBelowThreshold = (currentValue) => currentValue.length >= 10;
+  return arr.some(isBelowThreshold);
 }
 function noLongWords(arr) {
   const isBelowThreshold = (currentValue) => currentValue.length <= 7;
