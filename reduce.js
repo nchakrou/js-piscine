55333a00a285be2b6d0c13a668c997855f370b93
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-06 11:45:37 +0100

    first

--- reduce.js
+++ reduce.js
@@ -10,16 +10,15 @@ function foldRight(arr, fn, acc = 0) {
   }
   return acc;
 }
-function reduce(arr, fn, acc = 0) {
-  for (let i = 0; i < arr.length; i++) {
+function reduce(arr, fn, acc = arr[0]) {
+  for (let i = 1; i < arr.length; i++) {
     acc = fn(acc, arr[i]);
   }
   return acc;
 }
-function reduceRight(arr, fn, acc = 0) {
-  for (let i = arr.length - 1; i >= 0; i--) {
+function reduceRight(arr, fn, acc = arr[arr.length - 1]) {
+  for (let i = arr.length - 2; i >= 0; i--) {
     acc = fn(acc, arr[i]);
   }
-
   return acc;
 }
