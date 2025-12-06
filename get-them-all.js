7ab756a36eeeadb27350d3f5b4391cb7e8313394
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-06 18:40:23 +0100

    first

--- get-them-all.js
+++ get-them-all.js
@@ -9,12 +9,12 @@ const b = Array.from(document.body.querySelectorAll("*:not(.classical)"))
 return [a,b]
 }
 export const getActive = ()=>{
-const a = Array.from(document.getElementsByClassName("active"))
-const b = Array.from(document.querySelectorAll("*:not(.active)"))
+const a = Array.from(document.querySelectorAll("classical.active"))
+const b = Array.from(document.querySelectorAll("a:not(.classical.active)"))
 return [a,b]
 }
 export const getBonannoPisano = ()=>{
 const a = [document.getElementById("BonannoPisano")]
-const b = Array.from(document.body.querySelectorAll(".classical.active:not(#BonannoPisano)"))
+const b = Array.from(document.body.querySelectorAll("a.classical.active:not(#BonannoPisano)"))
 return [a,b]
 }
