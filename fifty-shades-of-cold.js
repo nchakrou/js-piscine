06774574eece726d770ba65d1b363e9d834826c7
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-07 18:31:24 +0100

    first

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,28 @@
+import { colors } from "./fifty-shades-of-cold.data.js";
+
+export function generateClasses() {
+  const l = document.createElement("style");
+  for (let i of colors) {
+    l.textContent += `.${i} { background: ${i};}\n`;
+  }
+  document.head.append(l);
+}
+export function generateColdShades() {
+    
+    for (let v of colors){
+        let i=document.createElement("div")
+        i.classList.add(v);
+        i.textContent=v
+        document.body.append(i)
+    }
+}
+
+export function choseShade(shad) {
+    let y = document.body.querySelectorAll("div")
+    
+    for (let v of y){
+        v.classList.remove(test[0])
+        v.classList.add(shad)
+
+    }
+}
