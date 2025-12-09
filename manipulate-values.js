4c173f6378d04bc73fe5133f4092d9346b112ced
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-09 14:25:41 +0100

    first

--- manipulate-values.js
+++ manipulate-values.js
@@ -1,34 +1,22 @@
-function filterValues(obj,fn){
-    let res ={};
-    for (let i in obj){
-        if (fn(obj[i])){
-            res[i]=obj[i]
-        }
+function filterValues(obj, fn) {
+  let res = {};
+  for (let i in obj) {
+    if (fn(obj[i])) {
+      res[i] = obj[i];
     }
-    return res
+  }
+  return res;
 }
-function mapValues(obj,fn){
-    let res ={};
-    for (let key in obj){
-        res[key]=fn(obj[key])
-    }
-    return res
+function mapValues(obj, fn) {
+  let res = {};
+  for (let key in obj) {
+    res[key] = fn(obj[key]);
+  }
+  return res;
 }
-function reduceValues(obj,fn){
-       let acc = 0
-    for (let key in obj){
-        acc=fn(acc,obj[key])
-    }
-    return acc
+function reduceValues(obj, fn, acc = 0) {
+  for (let key in obj) {
+    acc = fn(acc, obj[key]);
+  }
+  return acc;
 }
-
-const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
-
-console.log(filterValues(nutrients, (nutrient) => nutrient <= 12))
-// output: { carbohydrates: 12, fat: 5 }
-
-console.log(mapValues(nutrients, (v) => v+1))
-// output: { carbohydrates: 13, protein: 21, fat: 6 }
-
-console.log(reduceValues(nutrients, (acc, cr) => acc + cr))
-// output: 37
