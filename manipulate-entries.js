921e176dd76a8cb3a1e395afa43e7cc5ba3c098e
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-10 18:24:15 +0100

    first

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -17,35 +17,29 @@ function mapEntries(obj, fn) {
 }
 function reduceEntries(obj, fn, acc = 0) {
   for (let [key, value] of Object.entries(obj)) {
-    acc = fn(acc,[key, value]);
+    acc = fn(acc, [key, value]);
   }
   return acc;
 }
 
 function totalCalories(obj) {
-  let res =0
-for (let key in obj){
-  res+=(obj[key]/100)*nutritionDB[key].calories
-}
-return res
-
-}
-function lowCarbs(obj){
-
- return filterEntries(obj,([a,b])=>(b/100)*nutritionDB[a].carbs<50)
-  
-}
-function cartTotal(obj){
-  let res ={};
-for (let key1 in obj){
-  let a = obj[key1]/100
-  res[key1]={}
-  for (let key2 in nutritionDB[key1]){
-   res[key1][key2]=nutritionDB[key1][key2]*a
-
-    
+  let res = 0;
+  for (let key in obj) {
+    res += (obj[key] / 100) * nutritionDB[key].calories;
   }
+  return res;
 }
-return res
+function lowCarbs(obj) {
+  return filterEntries(obj, ([a, b]) => (b / 100) * nutritionDB[a].carbs < 50);
 }
-
+function cartTotal(obj) {
+  let res = {};
+  for (let key1 in obj) {
+    let a = obj[key1] / 100;
+    res[key1] = {};
+    for (let key2 in nutritionDB[key1]) {
+      res[key1][key2] = Math.round(nutritionDB[key1][key2] * a);
+    }
+  }
+  return res;
+}
