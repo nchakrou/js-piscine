efc1fd5130ab40c8531b411a816c3425b5650ac5
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-10 21:53:20 +0100

    first

---
+++ deep-copy.js
@@ -0,0 +1,22 @@
+function deepCopy(arg) {
+  if (Array.isArray(arg)) {
+    let res = [];
+    for (let v of arg) {
+      if (Array.isArray(arg)) {
+        res.push(deepCopy(v));
+      } else {
+        res.push(v);
+      }
+    }
+  } else if (typeof arg == "object") {
+    let res = {};
+    for (let [k, v] of Object.entries(arg)) {
+      if (typeof v != "object") {
+        res[k] = v;
+      } else {
+        res[k] = deepCopy(v);
+      }
+    }
+  }
+  return arg;
+}
