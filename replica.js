dd9a8265a5ccb3ca11fe7a2ebeb16988b5555ed6
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-10 22:45:59 +0100

    first

--- replica.js
+++ replica.js
@@ -1,14 +1,17 @@
 function replica(tar, ...obj) {
   for (let key of obj) {
     for (let k in key) {
-      if (typeof tar[k]== "object" &&typeof key[k] == "object" && !Array.isArray(key[k]) && !Array.isArray(tar[k])) {
-      
-        replica(tar[key], key[k]);
-      } else {      
+      if (
+        typeof tar[k] == "object" &&
+        !Array.isArray(tar[k]) &&
+        typeof key[k] == "object" &&
+        !Array.isArray(key[k])
+      ) {
+        replica(tar[k], key[k]);
+      } else {
         tar[k] = key[k];
       }
     }
   }
   return tar;
 }
-
