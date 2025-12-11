b74c9b85a61bf4df94f8da5e2ad67de6fbe76f77
nabil chakroune <nchakrou@noreply.learn.zone01oujda.ma>
2025-12-11 14:25:28 +0000

    Update long-words.js

--- long-words.js
+++ long-words.js
@@ -1,28 +1,38 @@
-function longWords(arr){
-    for (let i of arr){
-       if (typeof i !="string"||i.length<5){
-        return false
-       }
-        
+function longWords(arr) {
+    console.log("l", arr);
+
+
+    for (let i of arr) {
+        if (typeof i != "string" || i.length < 5) {
+            return false
+        }
+
     }
     return true
 }
-function oneLongWord(arr){
-    for (let i of arr){
-        
-       if (typeof i =="string"&&i.length>=10){
-        return true
-       }
-        
+function oneLongWord(arr) {
+    console.log("o", arr);
+
+
+    for (let i of arr) {
+
+        if (typeof i == "string" && i.length >= 10) {
+            return true
+        }
+
     }
     return false
 }
-function noLongWords(arr){
-      for (let i of arr){
-       if (typeof i =="string"&&i.length>=7){
-        return false
-       }
-        
+function noLongWords(arr) {
+    console.log("n", arr);
+
+    for (let i of arr) {
+        if (typeof i == "string" && i.length > 7) {
+            return false
+        }
+
     }
     return true
 }
+
+
