9c7d6157979bf6d6b087966ce3e41ff69c63e78a
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-13 20:14:13 +0100

    first

--- debounce.js
+++ debounce.js
@@ -1,25 +1,26 @@
 function debounce(fn, delay) {
-    let timer
-    return (...args) => {
-        clearTimeout(timer)
-        timer = setTimeout(() => fn(...args), delay)
-    }
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
 }
 
-
 function opDebounce(fn, wait, leading = false) {
-    let timer
-    
-    return (...args) => {
-        const isFirstCall = !timer
-         clearTimeout(timer)
-        
-         timer = setTimeout(() => {
-            timer = null
-        }, wait)
-        
-        if (leading && isFirstCall) {
-            fn(...args)
-        }
+  let timer;
+
+  return (...args) => {
+    const isFirstCall = !timer;
+    clearTimeout(timer);
+
+    if (leading && isFirstCall) {
+      fn(...args);
     }
+    timer = setTimeout(() => {
+      if (!leading) {
+        fn(...args);
+      }
+      timer = null;
+    }, wait);
+  };
 }
