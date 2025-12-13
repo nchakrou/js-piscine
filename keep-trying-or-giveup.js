9416b1935213c86bbc9e3e47b9bbfffad4cb958f
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-13 17:55:00 +0100

    first

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,19 @@
+function retry(count, callback) {
+  return async function (...arg) {
+    for (let i = 0; i <= count; i++) {
+      try {
+        return await callback(...arg);
+      } catch (err) {
+        if (i >= count) throw err;
+      }
+    }
+  };
+}
+function timeout(delay, callback) {
+  return async function (...arg) {
+    const timer = new Promise((_, reject) =>
+      setInterval(() => reject(new Error("timeout")), delay)
+    );
+    return Promise.race(callback(...arg), timer);
+  };
+}
