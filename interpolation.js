df1cc23cc0fae34a91b0cb07a7c498e29fc2b7d2
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-13 13:23:52 +0100

    first

--- interpolation.js
+++ interpolation.js
@@ -1,18 +1,16 @@
 function interpolation({ step, start, end, callback, duration }) {
-  const amountToAdd = (end - start) / step;
-  const delay = duration / step;
-
-  let count = 0;
-  let currentPoint = start;
-
-  const timer = setInterval(() => {
-    if (count < step) {
-      const distance = count / step;
-      currentPoint = start + (count * amountToAdd)
-      callback([distance, currentPoint]);
-      count++;
-    } else {
-      clearInterval(timer);
-    }
-  }, delay);
+    const amountToAdd = (end - start) / step
+    const delay = duration / step
+    let count = 0
+    const timer = setInterval(() => {
+        if (count < step) {
+            const y = delay * (count + 1)
+            callback([start, y])
+        
+            start += amountToAdd
+            count++
+        } else {
+            clearInterval(timer)
+        }
+    }, wait)
 }
