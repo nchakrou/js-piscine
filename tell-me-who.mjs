389ae17d127c6ca67670ae23f747caaf7bc90c81
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-15 21:02:22 +0100

    first

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -2,13 +2,16 @@ import { readdir } from "fs/promises";
 
 const path = process.argv[2];
 const files = await readdir(path);
-files.sort((a, b) => b.localeCompare(a));
-let res = [];
+
+let rese = [];
+
 for (let i = 0; i < files.length; i++) {
   let res = files[i].split(".");
   const [firstname, lastname] = res[0].split("_");
-  res.push(`${lastname} ${firstname}`);
+  rese.push(`${lastname} ${firstname}`);
 }
-for (let i = 0; i < res.length; i++) {
-  console.log(`${i + 1}. ${res[i]}`);
+rese.sort();
+for (let i = 0; i < rese.length; i++) {
+  console.log(`${i + 1}. ${rese[i]}`);
 }
+
