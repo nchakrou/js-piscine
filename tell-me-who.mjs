ee745ad01062def47779a27ba9d37eab941399a3
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-15 20:51:21 +0100

    first

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -2,6 +2,7 @@ import {readdir} from "fs/promises"
  
 const path = process.argv[2]
 const files = await readdir(path)
+files.sort((a,b)=>a.localeCompare(b))
 for (let i =0 ;i<files.length;i++){
 let res = files[i].split(".")
     const [firstname, lastname] = res[0].split('_')
