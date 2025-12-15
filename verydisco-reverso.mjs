a19f9e6a19045adab2c868c05454fd039f3315d1
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-15 16:31:00 +0100

    first

---
+++ verydisco-reverso.mjs
@@ -0,0 +1,13 @@
+import {readFile} from 'fs/promises'
+
+let arch =  await readFile("verydisco.txt")
+arch = arch.toString().split(" ")
+let res = ""
+for (let i = 0; i < arch.length; i++) {
+  let mid = Math.ceil(arch[i].length / 2);
+  if (i != 0){
+    res+=" "
+  }
+  res+=arch[i].slice(mid)+arch[i].slice(0,mid)
+}
+console.log(res.toString());
