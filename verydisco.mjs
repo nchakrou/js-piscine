3e2034a6b5b30c591ffd7d0b1da3db4d8c7b96fb
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-15 15:01:34 +0100

    first

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,14 +1,13 @@
-let arch = process.argv.slice(2);
-if (arch.length == 1){
-    arch = arch.join(" ")
-    arch = arch.split(" ")
-}
-let res = ""
+let arch = process.argv[2];
+
+arch = arch.split(" ");
+
+let res = "";
 for (let i = 0; i < arch.length; i++) {
   let mid = Math.ceil(arch[i].length / 2);
-  if (i != 0){
-    res+=" "
+  if (i != 0) {
+    res += " ";
   }
-  res+=arch[i].slice(mid)+arch[i].slice(0,mid)
+  res += arch[i].slice(mid) + arch[i].slice(0, mid);
 }
 console.log(res);
