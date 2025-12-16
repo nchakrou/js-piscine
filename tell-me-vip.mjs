8d4f8859c8144cf3c65715963b483396256a1e8e
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-16 22:16:59 +0100

    first

---
+++ tell-me-vip.mjs
@@ -0,0 +1,22 @@
+import { readdir, readFile, writeFile } from 'fs/promises'
+
+const arg = process.argv[2]
+const files = await readdir(arg)
+const guests = []
+for (const file of files) {
+    const Path = dirPath+"/"+file
+    const content = await readFile(Path, 'utf8')
+    const data = JSON.parse(content)
+    
+    if (data.answer === 'yes') {
+        const name = file.split('.')[0]
+        const [firstname, lastname] = name.split('_')
+        guests.push(`${lastname} ${firstname}`)
+    }
+}
+
+guests.sort()
+
+const result = guests.map((guest, index) => `${index + 1}. ${guest}`).join('\n')
+
+await writeFile('vip.txt', result)
