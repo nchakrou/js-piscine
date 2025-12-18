3bd5a2acd22294d9253d0ab775f686d26204bf0b
nchakrou nchakrou <nabilchakroune2@gmail.com>
2025-12-18 15:30:54 +0100

    first

--- tell-it-cypher.mjs
+++ tell-it-cypher.mjs
@@ -2,7 +2,7 @@ import { readFile, writeFile } from "fs/promises";
 
 const file = await readFile(process.argv[2]);
 const action = process.argv[3];
-const filename = process.argv[3];
+const filename = process.argv[4];
 const outputfile = filename || (action === "encode" ? "cypher.txt" : "clear.txt");
 if (action === "encode") {
   await writeFile(outputfile, Buffer.from(file).toString("base64"));
